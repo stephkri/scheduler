6ba13126c4320bcbfef50cbbebbfa7b495d2f7d5
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-12 00:24:17 -0500

    Change hardcoded text to props variables

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,9 +9,9 @@ export default function InterviewerListItem(props) {
   return (<li className={itemClass} onClick={() => props.setInterviewer(props.name)}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  {props.name && props.name}
 </li>)
 };
