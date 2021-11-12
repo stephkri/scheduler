29f2b844b79033c7a56898f5a86533445565fdad
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-12 18:00:01 -0500

    Refactor DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -21,7 +21,7 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
   return (
-    <li className={itemClass} onClick={() => props.setDay(props.name)}>
+    <li className={itemClass} onClick={() => props.setDay}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
