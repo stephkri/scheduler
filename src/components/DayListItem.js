3be9c47f79bb962feb9e0fea2e392bb339c5f12e
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-11 23:02:41 -0500

    Make formatSpots function to make all tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,6 +4,17 @@ import "./DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const formatSpots = function(spots) {
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    }
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+    if (!spots) {
+      return "no spots remaining";
+    }
+  }
 
   const itemClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
@@ -12,7 +23,7 @@ export default function DayListItem(props) {
   return (
     <li className={itemClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
