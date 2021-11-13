46c1cc0b5ce56d80e7f2fc9ebf9e2109601aa297
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-13 01:54:40 -0500

    Refactor Status to take props

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -7,6 +7,6 @@ export default function Status(props) {
       src="images/status.png"
       alt="Loading"
     />
-    <h1 className="text--semi-bold">Deleting</h1>
+    <h1 className="text--semi-bold">{props.message}</h1>
   </main>);
 };
