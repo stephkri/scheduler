a44706dd7ca269aaee540a605755c2b2acf27150
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-13 01:20:55 -0500

    Add header story

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,3 +141,4 @@ storiesOf("Button", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
