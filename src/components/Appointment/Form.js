6b21195d62c3e11aa70992d459fbea73e379c976
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-13 17:16:57 -0500

    Add reset function

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,12 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  };
+
   return (<main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
       <form autoComplete="off">
@@ -25,8 +31,8 @@ export default function Form(props) {
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button confirm>Save</Button>
+        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button confirm onClick={props.onSave}>Save</Button>
       </section>
     </section>
   </main>);
