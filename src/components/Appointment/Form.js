2ea987906c61da9fcc4612a0c1f754367d88c00a
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-13 02:28:57 -0500

    Insert setStuff into form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,6 +3,8 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return (<main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
       <form autoComplete="off">
