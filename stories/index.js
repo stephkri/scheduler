49b6b1d907bdbd937f13b2032860ecbd3e16a3b3
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-13 02:15:20 -0500

    Import form into stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
 import Error from "components/Appointment/Error.js";
+import Form from "components/Appointment/Form.js";
 
 storiesOf("Button", module)
   .addParameters({
