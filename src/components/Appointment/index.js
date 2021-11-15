9f7a5d44cbab431bc8c50cabe7a8152e10fec2af
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-14 21:47:01 -0500

    Remove console log statement

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,7 +5,6 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  console.log(props);
   return (<article className="appointment">
     <Header time={props.time} />
     {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
