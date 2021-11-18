9032a7d3578fc14ea5e94694b692c44d83f57348
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-18 02:27:35 -0500

    Add successful code to getAppointmentsForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,15 @@
 export function getAppointmentsForDay(state, day) {
-
+  const arr = [];
+  let correctDay = false;
+  for (const oneDay of state.days) {
+    if (oneDay.name === day) {
+      correctDay = oneDay;
+    }
+  }
+  if (correctDay) {
+    for (const appointment of correctDay.appointments) {
+      arr.push(state.appointments[appointment]);
+    }
+  }
+  return arr;
 };
