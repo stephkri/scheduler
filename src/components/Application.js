498b395afcf64997fbe25e901dd81092a43e3c0a
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-19 02:31:26 -0500

    Refactor setState in Promise

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,9 +31,7 @@ export default function Application(props) {
     axios.get('/api/appointments')
   ])
   .then(all => {
-    const daysData = all[0].data;
-    const apptsData = all[1].data;
-    setState(prev => ({...prev, days: daysData, appointments: apptsData}))
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
   })
   .catch(e => console.log(e));
 
