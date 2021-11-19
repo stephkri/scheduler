421fd98157acf78d7d74f1a48b95d4655b650002
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-19 02:26:32 -0500

    Delete commented out code

--- src/components/Application.js
+++ src/components/Application.js
@@ -15,68 +15,16 @@ const interviewers = [
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
 
-/*
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-*/
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
     appointments: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = day => setState({ ...state, day });
-  /*
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
-  useEffect(() => {
-    axios.get('/api/days').then(response => {
-      setDays(response.data);
-    })
-    .catch(e => console.log(e));
-  }, [])
-  */
 
   Promise.all([
     axios.get('/api/days'),
