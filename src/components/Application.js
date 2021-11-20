e01a164567ca381ff79fd7aa0f29df201e590e36
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 16:19:37 -0500

    Add interviewers to Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -29,10 +29,12 @@ export default function Application(props) {
 
   Promise.all([
     axios.get('/api/days'),
-    axios.get('/api/appointments')
+    axios.get('/api/appointments'),
+    axios.get('/api/interviewers')
   ])
   .then(all => {
-    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+    console.log(all[2]);
+    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
   })
   .catch(e => console.log(e));
 
