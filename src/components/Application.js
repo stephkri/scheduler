0d9b8ffbb2a566d8d7c34462be165b3068739455
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 16:14:12 -0500

    Add empty interviewers object to setState

--- src/components/Application.js
+++ src/components/Application.js
@@ -19,7 +19,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
