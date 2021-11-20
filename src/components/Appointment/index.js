dc50234dc86d63db18f5655436efb428b2432bac
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 17:51:02 -0500

    Import useVisualMode into Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,7 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   return (<article className="appointment">
