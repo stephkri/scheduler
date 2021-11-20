e75c26c4abf3a11e94ce604f8782a964cef110fd
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 16:26:44 -0500

    Add interviewers data to test state object

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -27,6 +27,18 @@ const state = {
       time: "4pm",
       interview: { student: "Chad Takahashi", interviewer: 2 }
     }
+  },
+  interviewers: {
+    "1": {  
+      "id": 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    }
   }
 };
 
@@ -55,3 +67,6 @@ test("getAppointmentsForDay returns an empty array when the day is not found", (
   const result = getAppointmentsForDay(state, "Wednesday");
   expect(result.length).toEqual(0);
 });
+
+/* *************** */
+
