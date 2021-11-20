bb0e40a0d9b5b82bc703a6742cdff21ef08586ba
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 17:51:52 -0500

    Make EMPTY and SHOW constants in Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,8 @@ import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
   return (<article className="appointment">
     <Header time={props.time} />
     {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
