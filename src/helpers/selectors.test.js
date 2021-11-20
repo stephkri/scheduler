62344edb01a1a8bfa9c1e376bbdf5a7df9bd59ae
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-20 16:27:12 -0500

    Add tests for getInterview

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -70,3 +70,21 @@ test("getAppointmentsForDay returns an empty array when the day is not found", (
 
 /* *************** */
 
+test("getInterview returns an object with the interviewer data", () => {
+  const result = getInterview(state, state.appointments["3"].interview);
+  expect(result).toEqual(
+    expect.objectContaining({
+      student: expect.any(String),
+      interviewer: expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        avatar: expect.any(String)
+      })
+    })
+  );
+});
+
+test("getInterview returns null if no interview is booked", () => {
+  const result = getInterview(state, state.appointments["2"].interview);
+  expect(result).toBeNull();
+});
