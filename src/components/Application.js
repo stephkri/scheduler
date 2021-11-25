fdf239ac6d85fadbdad4bc2b070e4a12ffa31f22
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-24 22:59:59 -0500

    Fix useEffect and component bugs

--- src/components/Application.js
+++ src/components/Application.js
@@ -40,16 +40,17 @@ export default function Application(props) {
     setState({ ...state, appointments });
   };
 
-  Promise.all([
-    axios.get('/api/days'),
-    axios.get('/api/appointments'),
-    axios.get('/api/interviewers')
-  ])
-  .then(all => {
-    //console.log(all[2]);
-    setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-  })
-  .catch(e => console.log(e));
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ])
+    .then(all => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    })
+    .catch(e => console.log(e));
+  }, []);
 
   return (
     <main className="layout">
