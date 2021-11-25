06e55e309d791fa8582471a720774ac92a94430c
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-25 00:59:23 -0500

    Complete Create Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -37,7 +37,9 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
-    setState({ ...state, appointments });
+    return axios.put(`/api/appointments/${id}`, appointment)
+    .then(() => setState({ ...state, appointments }))
+    .catch(e => console.log(e));
   };
 
   useEffect(() => {
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,12 +4,14 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -18,18 +20,21 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview);
-    transition(SHOW);
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(transition(SHOW))
+    .catch(e => console.log(e))
   };
   return (<article className="appointment">
     <Header time={props.time} />
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-    {mode === SHOW && (
+    {mode === SHOW && props.interview && (
       <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
       />
     )}
     {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save} />}
+    {mode === SAVING && <Status message="Saving..." />}
   </article>);
 };
