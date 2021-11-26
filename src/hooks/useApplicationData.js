cf63222dea60ea05b00651fe9b3e40edabf6e7ab
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-26 01:06:24 -0500

    Paste cancelInterview into useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -35,4 +35,17 @@ export default function useApplicationData() {
     return axios.put(`/api/appointments/${id}`, appointment)
     .then(() => setState({ ...state, appointments }));
   };
+
+  const cancelInterview = function(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`/api/appointments/${id}`)
+    .then(() => setState({ ...state, appointments }));
+  };
 };
