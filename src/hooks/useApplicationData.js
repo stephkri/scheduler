02617ec993f911f93009a9dee909e2115eab06d4
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-26 01:04:48 -0500

    Paste useEffect with axios data into useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -10,4 +10,16 @@ export default function useApplicationData() {
   });
 
   const setDay = day => setState({ ...state, day });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ])
+    .then(all => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    })
+    .catch(e => console.log(e));
+  }, []);
 };
