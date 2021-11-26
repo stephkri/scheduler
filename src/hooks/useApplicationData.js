0d37aea695a82e396665247b6102d9996408e8e2
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-26 01:00:09 -0500

    Paste state and setState into useApplicationData

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,5 +2,10 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
 };
