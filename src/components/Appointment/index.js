58e45193326d52050a830773f1fa32aed1fc75e7
Stéphane Krims <stephane.krims@hotmail.ca>
2021-11-25 19:35:14 -0500

    Add ERROR_DELETE const

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -18,6 +18,7 @@ export default function Appointment(props) {
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
   const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
