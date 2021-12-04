2a8688193055ed5fc4f48c81522d29e55860e49b
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-03 23:30:53 -0500

    Import PropTypes into InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "./InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   return (<section className="interviewers">
