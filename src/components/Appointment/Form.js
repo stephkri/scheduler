ba82506af144c6e46b81a7d09ccdbe4ce51e0df1
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-04 20:55:38 -0500

    Add useState for error

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setStudent("");
