07d8ee19463e71a5f7c10ed136831d7b4f33d361
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-04 19:57:35 -0500

    Insert first test into Appointment test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Application from "components/Application";
+import Appointment from "../Appointment/index";
 
 describe("Appointment", () => {
-
+  it("renders without crashing", () => {
+    render(<Appointment />)
+  })
 });
