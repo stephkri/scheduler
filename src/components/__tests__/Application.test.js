209e868605572df6c856a6f4a737eb45e7f1870f
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-06 12:30:41 -0500

    Import prettyDOM in Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
