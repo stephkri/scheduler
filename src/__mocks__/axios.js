4927622159ecf5d64897804258feeb854e6e57a1
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-06 16:02:53 -0500

    Add delete property to mock axios function

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -73,5 +73,11 @@ export default {
       status: 204,
       statusText: "No Content"
     })
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
   })
 };
