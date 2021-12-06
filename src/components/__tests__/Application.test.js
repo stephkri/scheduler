4111a99cb0f2e076a6f94dbae94538ad3c7f562e
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-06 12:39:04 -0500

    Add waitForElement, import getByText so everything works

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -13,8 +13,9 @@ describe("Application", () => {
     fireEvent.click(getByText("Tuesday"));
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
-  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
     console.log(prettyDOM(container));
   });
 });
