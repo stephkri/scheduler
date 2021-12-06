1d5d5d677bddb85ca957841b92224ce9676087a9
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-06 12:30:23 -0500

    Add second test and container to Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,9 +6,15 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+describe("Application", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+    const { container } = render(<Application />);
+    console.log(container);
+  });
 });
