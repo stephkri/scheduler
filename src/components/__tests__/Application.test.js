73eb559523d7060c0122227d9e9d6274551cf6e2
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-06 23:53:25 -0500

    Expect the error message for delete

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -110,5 +110,6 @@ describe("Cancel test", () => {
     fireEvent.click(getByText(cohenAppt, "Confirm"));
     expect(getByText(cohenAppt, "Deleting...")).toBeInTheDocument();
     await waitForElement(() => getByTestId(cohenAppt, "error"));
+    expect(queryByText(appointment, "Could not cancel the appointment.")).toBeTruthy();
   })
 });
