1f06185039755b226e747aa66969620578e83f0a
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-10 17:26:14 -0500

    Refactor click test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -4,9 +4,8 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get("li").contains("Tuesday").click();
-    cy.get("li")
-    .contains("Tuesday")
+    cy.contains("li", "Tuesday")
+    .click()
     .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
