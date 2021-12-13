820dc7dabebd7622d3f9adf3477085305261946d
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-13 13:58:32 -0500

    Add assertions that new appointment shows up

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -6,5 +6,7 @@ describe("Appointment", () => {
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
     cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
