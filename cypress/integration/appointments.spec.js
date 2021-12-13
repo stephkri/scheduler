c2b4cb93d0d5745b51a93e6105653282d5d93fac
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-13 13:49:52 -0500

    Debug click of Add

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,7 +1,7 @@
 describe("Appointment", () => {
   it("should book an interview", () => {
     cy.visit("/").contains("Monday");
-    cy.get("alt", "Add").first().click();
+    cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   });
 });
