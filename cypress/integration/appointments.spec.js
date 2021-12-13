93668dc89400ec25ece30932bf2ef98e5b3dc264
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-13 14:00:19 -0500

    Refactor describe with beforeEach

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,7 +1,9 @@
 describe("Appointment", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
     cy.visit("/").contains("Monday");
+  })
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
