aca73bf89db162f0d6a18d52f23cd8dd92ceb2bf
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-13 13:51:38 -0500

    Click on interviewer

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -3,5 +3,6 @@ describe("Appointment", () => {
     cy.visit("/").contains("Monday");
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt=Sylvia Palmer]").click();
   });
 });
