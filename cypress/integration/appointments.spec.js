fdd9dc2aaa9ac02a22b7546f39a95fb392a084ba
Stéphane Krims <stephane.krims@hotmail.ca>
2021-12-13 13:47:21 -0500

    Add student name in test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,5 +2,6 @@ describe("Appointment", () => {
   it("should book an interview", () => {
     cy.visit("/").contains("Monday");
     cy.get("alt", "Add").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
   });
 });
